431e9bb9b91dbe6cc0ab04ffe7b5db346f75736c
atsuo yamada <yamada-git2@tir.jp>
2026-04-09 16:45:29 +0900

    feat: improve HiccupTree typing by introducing HiccupChild by mac

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,25 @@
 // Tag can be a string (HTML tag) or a functional component
 export type HiccupTag = string | ((...args: any[]) => (HiccupTree | HTMLElement));
 
-export type HiccupTree = [HiccupTag, ...any[]];
+export type HiccupChild =
+  | Record<string, any>
+  | string
+  | number
+  | boolean
+  | null
+  | undefined
+  | HTMLElement
+  | HiccupTree
+  | HiccupChild[];
+
+export type HiccupTree = [HiccupTag, ...HiccupChild[]];
 
 /**
  * Recursively sets attributes/properties on an HTMLElement.
  */
-export function setAttr(htmlObj: any, attrObj: Record<string, any>): void;
+export declare function setAttr(htmlObj: any, attrObj: Record<string, any>): void;
 
 /**
  * Builds an HTMLElement tree from a nested array (Hiccup format).
  */
-export function build(treeArray: HiccupTree): HTMLElement;
+export declare function build(treeArray: HiccupTree): HTMLElement;
